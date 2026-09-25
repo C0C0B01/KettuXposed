6ed9625998e0f9dce0b1ca8fd478fbd16b67ec16
cocobo1 <CocoboTheOne@gmail.com>
2026-09-25 18:40:48 +0000

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cocobo1.pupu.xposed"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1410
-        versionName = "1.4.1"
+        versionCode = 1420
+        versionName = "1.4.2"
     }
 
     buildTypes {
